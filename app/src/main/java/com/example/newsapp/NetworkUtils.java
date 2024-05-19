acfb28dd91c7798f1c68464549877bae8bde7abb
Neel <neeltailor3@gmail.com>
2024-05-19 23:58:40 +0530

    Newsdata.io API integration

---
+++ app/src/main/java/com/example/newsapp/NetworkUtils.java
@@ -0,0 +1,56 @@
+package com.example.newsapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUtils {
+
+    public static String getJSONFromAPI(String urlString) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String jsonResponse = null;
+
+        try {
+            URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuilder buffer = new StringBuilder();
+            if (inputStream == null) {
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line).append("\n");
+            }
+
+            if (buffer.length() == 0) {
+                return null;
+            }
+            jsonResponse = buffer.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return jsonResponse;
+    }
+}
--- app/src/main/java/com/example/newsapp/NewsFeedActivity.java
+++ app/src/main/java/com/example/newsapp/NewsFeedActivity.java
@@ -1,10 +1,17 @@
 package com.example.newsapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class NewsFeedActivity extends AppCompatActivity {
+    private TextView tvNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,41 @@ public class NewsFeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_feed);
 
+        tvNews = findViewById(R.id.tvNews);
+
+        String API_ENDPOINT = "https://newsdata.io/api/1/news?" +
+                "country=in&" +
+                "language=en&" +
+                "category=education,environment,science,technology,world&" +
+                "apikey=" + getString(R.string.newsdataio_apikey);
+
+        new FetchNewsTask().execute(API_ENDPOINT);
     }
+
+    private class FetchNewsTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            return NetworkUtils.getJSONFromAPI(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String jsonResponse) {
+            if (jsonResponse != null) {
+                try {
+                    JSONObject apiResponse = new JSONObject(jsonResponse);
+                    JSONArray jsonArray = apiResponse.getJSONArray("results");
+                    StringBuilder newsBuilder = new StringBuilder();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject newsObject = jsonArray.getJSONObject(i);
+                        String title = newsObject.getString("title");
+                        newsBuilder.append("Title: ").append(title).append("\n");
+                    }
+                    tvNews.setText(newsBuilder.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
